3e355aa99332f25dd7ba4597d19ac0809f835ec3
James Corey <jmc@surround.io>
2016-11-16 13:13:31 -0800

    ninja

---
+++ include/ellis/private/using.hpp
@@ -0,0 +1,38 @@
+/*
+ * @file private/using.hpp
+ *
+ * @brief Ellis public C header.
+ *
+ * This is the C header for Ellis, which implements the standalone Ellis data
+ * interaction routines.
+ */
+
+#pragma once
+#ifndef ELLIS_PRIVATE_USING_H_
+#define ELLIS_PRIVATE_USING_H_
+
+
+#include <algorithm>
+#include <deque>
+#include <list>
+#include <map>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+
+using std::deque;
+using std::function;
+using std::list;
+using std::map;
+using std::pair;
+using std::shared_ptr;
+using std::string;
+using std::unique_ptr;
+using std::unordered_map;
+using std::vector;
+
+
+#endif /* ELLIS_PRIVATE_USING_H_ */
