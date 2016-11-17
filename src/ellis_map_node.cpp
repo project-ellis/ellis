9d6bc812772d4a5dcc6dd01505e3b65dc24431bd
James Corey <jmc@surround.io>
2016-11-17 15:24:57 -0800

    fix miss behavior

--- src/ellis_map_node.cpp
+++ src/ellis_map_node.cpp
@@ -12,9 +12,22 @@ ellis_map_node::~ellis_map_node()
   /* Do nothing; destruction is handled by ellis_node. */
 }
 
+
 ellis_node & ellis_map_node::operator[](const std::string &key)
 {
-  auto it = m_node.m_blk->m_map.find(key);
+  return const_cast<ellis_node&>(
+      (*(static_cast<const ellis_map_node*>(this)))[key]);
+}
+
+
+const ellis_node & ellis_map_node::operator[](const std::string &key) const
+{
+  const auto it = m_node.m_blk->m_map.find(key);
+  if (it == m_node.m_blk->m_map.end()) {
+    auto added = m_node.m_blk->m_map.emplace(key, ellis_node(ellis_type::NIL));
+    assert(added.second == true);
+    return added.first->second;
+  }
   return it->second;
 }
 
