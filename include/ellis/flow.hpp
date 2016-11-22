bad451c63bcb020ea66175306aabaf752c0db57b
Martin Kelly <martin@surround.io>
2016-11-22 14:29:22 -0800

    include: encode/decode API wip

---
+++ include/ellis/flow.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file flow.hpp
+ *
+ * @brief Ellis Flow public C++ header.
+ *
+ * This is the C++ header for Ellis Flow, which implements Ellis encoders and
+ * decoders.
+ */
+
+#pragma once
+#ifndef ELLIS_FLOW_HPP_
+#define ELLIS_FLOW_HPP_
+
+
+namespace ellis {
+namespace flow {
+
+}  /* namespace flow */
+}  /* namespace ellis */
+
+#endif  /*
