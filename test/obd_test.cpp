19d93370a4927cbea9780794cf612aef10f21a32
Martin Kelly <martin@surround.io>
2016-12-13 10:02:36 -0800

    obdtest: use ELLIS_ASSERT instead of assert

--- test/obd_test.cpp
+++ test/obd_test.cpp
@@ -1,9 +1,9 @@
 #undef NDEBUG
-#include <assert.h>
 #include <ellis/codec/elm327.hpp>
 #include <ellis/core/array_node.hpp>
 #include <ellis/core/map_node.hpp>
 #include <ellis/private/codec/obd/pid.hpp>
+#include <ellis/core/system.hpp>
 #include <ellis/private/using.hpp>
 #include <test/assert.hpp>
 
@@ -13,30 +13,30 @@ int main() {
   using namespace ellis::test;
 
   /* OBD. */
-  assert(*get_mode_string(0x7F) == "unknown");
-  assert(get_mode_string(0x00) == nullptr);
-  assert(*get_mode_string(0x41) == "current");
-  assert(*get_mode_string(0x42) == "freeze");
-  assert(get_mode_string(0xFF) == nullptr);
+  ELLIS_ASSERT(*get_mode_string(0x7F) == "unknown");
+  ELLIS_ASSERT(get_mode_string(0x00) == nullptr);
+  ELLIS_ASSERT(*get_mode_string(0x41) == "current");
+  ELLIS_ASSERT(*get_mode_string(0x42) == "freeze");
+  ELLIS_ASSERT(get_mode_string(0xFF) == nullptr);
 
-  assert(get_pid_string(0xFF) == nullptr);
-  assert(string(get_pid_string(0x0D)) == "vehicle_speed");
+  ELLIS_ASSERT(get_pid_string(0xFF) == nullptr);
+  ELLIS_ASSERT(string(get_pid_string(0x0D)) == "vehicle_speed");
 
-  assert(dbl_equal(decode_value(0x05, 0xAB000000), 0xAB - 40));
-  assert(dbl_equal(decode_value(0x05, 0xAB123456), 0xAB - 40));
-  assert(dbl_equal(decode_value(0x05, 0x00000000), -40));
+  ELLIS_ASSERT(dbl_equal(decode_value(0x05, 0xAB000000), 0xAB - 40));
+  ELLIS_ASSERT(dbl_equal(decode_value(0x05, 0xAB123456), 0xAB - 40));
+  ELLIS_ASSERT(dbl_equal(decode_value(0x05, 0x00000000), -40));
 
-  assert(decode_value(0x0C, 0xABCD0000) == (0xAB*256 + 0xCD)/4.0);
-  assert(decode_value(0x0C, 0xABCD1234) == (0xAB*256 + 0xCD)/4.0);
+  ELLIS_ASSERT(decode_value(0x0C, 0xABCD0000) == (0xAB*256 + 0xCD)/4.0);
+  ELLIS_ASSERT(decode_value(0x0C, 0xABCD1234) == (0xAB*256 + 0xCD)/4.0);
 
-  assert(dbl_equal(decode_value(0x0D, 0xAB000000), 0xAB));
-  assert(dbl_equal(decode_value(0x0D, 0xAB123456), 0xAB));
+  ELLIS_ASSERT(dbl_equal(decode_value(0x0D, 0xAB000000), 0xAB));
+  ELLIS_ASSERT(dbl_equal(decode_value(0x0D, 0xAB123456), 0xAB));
 
-  assert(decode_value(0x10, 0xABCD0000) == (0xAB*256 + 0xCD)/100.0);
-  assert(decode_value(0x10, 0xABCD1234) == (0xAB*256 + 0xCD)/100.0);
+  ELLIS_ASSERT(decode_value(0x10, 0xABCD0000) == (0xAB*256 + 0xCD)/100.0);
+  ELLIS_ASSERT(decode_value(0x10, 0xABCD1234) == (0xAB*256 + 0xCD)/100.0);
 
-  assert(decode_value(0x14, 0xAB000000) == 0xAB / 200.0);
-  assert(decode_value(0x14, 0xAB123456) == 0xAB / 200.0);
+  ELLIS_ASSERT(decode_value(0x14, 0xAB000000) == 0xAB / 200.0);
+  ELLIS_ASSERT(decode_value(0x14, 0xAB123456) == 0xAB / 200.0);
 
   /* ELM327. */
   {
@@ -44,15 +44,15 @@ int main() {
     const byte buf[] = "41 05 B9\n";
     size_t count = sizeof(buf) - 1;
     decoding_status status = dec.consume_buffer(buf, &count);
-    assert(status == decoding_status::END);
-    assert(count == 0);
-    assert(dec.extract_error() == nullptr);
+    ELLIS_ASSERT(status == decoding_status::END);
+    ELLIS_ASSERT(count == 0);
+    ELLIS_ASSERT(dec.extract_error() == nullptr);
     node n = *dec.extract_node();
-    assert(n.as_array().length() == 1);
+    ELLIS_ASSERT(n.as_array().length() == 1);
     const map_node &m = n.as_array()[0].as_map();
-    assert(m["mode"] == "current");
-    assert(m["pid"] == "engine_coolant_temp");
-    assert(dbl_equal(m["value"], 0xB9 - 40));
+    ELLIS_ASSERT(m["mode"] == "current");
+    ELLIS_ASSERT(m["pid"] == "engine_coolant_temp");
+    ELLIS_ASSERT(dbl_equal(m["value"], 0xB9 - 40));
   }
 
   {
@@ -60,15 +60,15 @@ int main() {
     const byte buf[] = "41 0C 08 1B\n";
     size_t count = sizeof(buf) - 1;
     decoding_status status = dec.consume_buffer(buf, &count);
-    assert(status == decoding_status::END);
-    assert(count == 0);
-    assert(dec.extract_error() == nullptr);
+    ELLIS_ASSERT(status == decoding_status::END);
+    ELLIS_ASSERT(count == 0);
+    ELLIS_ASSERT(dec.extract_error() == nullptr);
     node n = *dec.extract_node();
-    assert(n.as_array().length() == 1);
+    ELLIS_ASSERT(n.as_array().length() == 1);
     const map_node &m = n.as_array()[0].as_map();
-    assert(m["mode"] == "current");
-    assert(m["pid"] == "engine_rpm");
-    assert(dbl_equal(m["value"], (0x08*256 + 0x1B)/4.0));
+    ELLIS_ASSERT(m["mode"] == "current");
+    ELLIS_ASSERT(m["pid"] == "engine_rpm");
+    ELLIS_ASSERT(dbl_equal(m["value"], (0x08*256 + 0x1B)/4.0));
   }
 
   {
@@ -76,8 +76,8 @@ int main() {
     const byte buf[] = "";
     size_t count = sizeof(buf) - 1;
     decoding_status status = dec.consume_buffer(buf, &count);
-    assert(status == decoding_status::ERROR);
-    assert(dec.extract_error() != nullptr);
+    ELLIS_ASSERT(status == decoding_status::ERROR);
+    ELLIS_ASSERT(dec.extract_error() != nullptr);
   }
 
   {
@@ -85,8 +85,8 @@ int main() {
     const byte buf[] = "\n";
     size_t count = sizeof(buf) - 1;
     decoding_status status = dec.consume_buffer(buf, &count);
-    assert(status == decoding_status::ERROR);
-    assert(dec.extract_error() != nullptr);
+    ELLIS_ASSERT(status == decoding_status::ERROR);
+    ELLIS_ASSERT(dec.extract_error() != nullptr);
   }
 
   return 0;
