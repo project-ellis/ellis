d1f03fde1939c71863807d5cc728116b5c3d7e30
James Corey <jmc@surround.io>
2016-12-16 07:44:51 -0800

    cleaned up header

--- include/ellis/codec/json.hpp
+++ include/ellis/codec/json.hpp
@@ -13,69 +13,23 @@
 #include <ellis/core/node.hpp>
 #include <ellis/core/stream_decoder.hpp>
 #include <ellis/core/stream_encoder.hpp>
-#include <sstream>
 
 namespace ellis {
 
 
+class json_parser;
+class json_tokenizer;
+
 class json_decoder : public stream_decoder {
 
-  enum class doc_state {
-    INIT,
-    MAP_GOT_OPEN,
-    MAP_GOT_KEY,
-    MAP_GOT_COLON,
-    MAP_GOT_VAL,
-    MAP_GOT_COMMA,
-    ARRAY_GOT_OPEN,
-    ARRAY_GOT_VAL,
-    ARRAY_GOT_COMMA,
-    GOT_VAL,
-  };
-
-  enum class token_type {
-    LEFT_CURLY,
-    RIGHT_CURLY,
-    COLON,
-    COMMA,
-    LEFT_SQUARE,
-    RIGHT_SQUARE,
-    STRING,
-    INTEGER,
-    REAL,
-    BAREWORD,
-  };
-
-  enum class token_state {
-    INIT,
-    STRING,
-    QUOTE,
-    QUOTE_U1,
-    QUOTE_U2,
-    QUOTE_U3,
-    QUOTE_U4,
-    INT,
-    FRAC,
-    EXP,
-    COMMENTSLASH2,
-    COMMENT,
-    END,
-  };
-
-  int m_sign;
-  int m_digitcount;
-  int64_t m_int;
   decoding_status m_status;
-  std::vector<std::unique_ptr<node>> m_stack;
-  token_state m_tokstate;
-  token_type m_toktype;
-  std::ostringstream m_txt;
+  std::unique_ptr<json_tokenizer> m_toker;
+  std::unique_ptr<json_parser> m_parser;
   std::unique_ptr<err> m_err;
-
-  void _clear_txt();
 
 public:
   json_decoder();
+  ~json_decoder();
   decoding_status consume_buffer(
       const byte *buf,
       size_t *bytecount) override;
@@ -86,12 +40,10 @@ public:
 
 
 class json_encoder : public stream_encoder {
-  std::stringstream m_obuf;
   std::unique_ptr<err> m_err;
   size_t m_obufpos = 0;
   size_t m_obufend = 0;
 
-  void _clear_obuf();
 public:
   json_encoder();
   encoding_status fill_buffer(
